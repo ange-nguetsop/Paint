87848182990ae9e36be4745867bf7eea962c8688
ange-Nguetsop <nguetsopange@yahoo.com>
2024-01-23 02:21:06 +0100

    I've added the option to erase the image. To do this, hold down the right mouse button and move over the area to be erased. You can also change the size of the eraser using the mouse wheel.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 startPoint = (0,0)
 endPoint = (0,0)
 drawing = False
+erase = False
 drawing_color = (0,0,0)  # Couleur de dessin initiale (noir)
 thickness = 5
 
@@ -19,17 +20,30 @@ cv2.createTrackbar('B','image',0,255,nothing)
 
 # Fonction pour dessiner
 def draw(event, x, y, flags, param):
-    global startPoint, endPoint, drawing_color, drawing, thickness
+    global startPoint, endPoint, drawing_color, drawing, thickness, erase
     if event == cv2.EVENT_LBUTTONDOWN:
         startPoint = (x, y)
         drawing = True
+        erase = False
+    elif event == cv2.EVENT_RBUTTONDOWN:
+        startPoint = (x, y)
+        erase = True
+        drawing = False
     elif event == cv2.EVENT_MOUSEMOVE:
         if drawing:
             endPoint = (x, y)
             cv2.line(img, pt1=startPoint, pt2=endPoint, color=drawing_color, thickness=thickness)
             startPoint = endPoint
+        if erase:
+            endPoint = (x, y)
+            cv2.line(img, pt1=startPoint, pt2=endPoint, color=(255,255,255), thickness=thickness)
+            startPoint = endPoint
+
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
+    elif event == cv2.EVENT_RBUTTONUP:
+        erase = False
+
     elif event == cv2.EVENT_MOUSEWHEEL:
         if flags > 0:  # Molette vers le haut
             thickness += 1
